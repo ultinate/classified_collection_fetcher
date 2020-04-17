84ca3c0d5c03841ac87101c43331852e754097b8
Nathanael Wettstein <nathanael.wettstein@gmail.com>
2020-04-17 06:42:21 +0200

    Add license, more help text, multiple fixes.

--- README.md
+++ README.md
@@ -1,12 +1,57 @@
-Goal
-====
+# Goal
 
-Fetch information about documents (laws).
+Fetch information about legal documents from [https://www.admin.ch/gov/de/start/bundesrecht/systematische-sammlung.html](admin.ch classified collection).
 
-Usage
-=====
+Provides some sort of "API for the poor". Works by parsing HTML pages.
 
-For example, call:
+Partly inspired by <https://github.com/syzer/sbb-blog-legal-hack-2017>.
+
+
+# Installation
+
+You need Python 3 installed locally. The following instructions apply for Linux.For other OSes, adjust to taste (e.g. `./env/Script/activate` on Windows instead of `source env/bin/activate`).
+
 ```
-cat ids.txt | xargs python sr_record.py
+python3 -m venv env
+source env/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+python sr_record.py -h
 ```
+
+
+# Usage examples
+
+Retrieve a document (e.g. "COVID-19 Verordnung 2"):
+
+```
+python sr_record.py 818.101.24
+```
+
+Retrieve info about a list of documents, but don't download the PDFs.
+```
+cat ids.txt | xargs python sr_record.py --print_only
+```
+
+
+# License
+MIT License, see `LICENSE`.
+
+
+# Keywords
+
+Keywords (DE): Gesetz, Verordnung, Systematische Rechtssammlung (SR), admin.ch
+
+Keywords (FR): loi, ordonnance, recueil systématique du droit fédéral (RS), admin.ch
+
+Keywords (IT): legge, ordinanza, raccolta sistematica del diritto federale (RS), admin.ch
+
+
+# TODO
+
+  * Choose language for downloads (currently: PDFs are downloaded in German).
+  * Make available as pip package on Pypi.org. Prerequisite: Have a catchy name.
+  * Provide history for a record.
+  * Provide change list for a record.
+  * Monitor changelog (e.g. RSS) for relevant changes.
+
--- sr_record.py
+++ sr_record.py
@@ -1,51 +1,26 @@
 import argparse
+import logging
 import os
 import re
 
 from bs4 import BeautifulSoup
 import requests
 
-
-sr_ids = [
-   # "818.101.24",  # COVID-19 Verordnung 2
-   "170.32",
-   "172.010",
-   "172.220.1",
-   "235.1",
-   "510.10",
-   "510.518",
-   "510.91",
-   "943.03",
-   "120.423",
-   "120.4",
-   "152.1",
-   "172.010.1",
-   "172.010.442",
-   "172.010.58",
-   "172.010.59",
-   "172.214.1",
-   "172.220.111.31",
-   "172.220.111.3",
-   "235.11",
-   "510.211.2",
-   "510.215",
-   "510.411",
-   "510.411",
-   "510.518.1",
-   "510.911",
-   "514.20",
-   "943.032.1",
-   "943.032.1",
-   "943.032",
-]
 search_url = "https://www.admin.ch/opc/search/?text={}"
-index_url = r"https://www.admin.ch/opc/de/classified-compilation/{}/index.html"
 latest_update_string = r"\(Stand am ([^<]*)\)<"
 is_active_string = "Dieser Text ist in Kraft"
 download_dir = "downloads"
+logger = logging.getLogger("console")
+file_logger = logging.getLogger("file_logger")
+logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 
 class SrRecord(object):
+    """A legal document from admin.ch classified collection.
+
+    May be a federal law, directive or other regulation.
+    SR is short for "Systemematische Rechtssammlung".
+    """
 
     def __init__(self, sr_id):
         self.sr_id = sr_id
@@ -55,7 +30,7 @@ class SrRecord(object):
         r = requests.get(url)
         soup = BeautifulSoup(r.text, 'html.parser')
         self.url = r.url
-        self.last_updated = self.get_last_update_date(soup)
+        self.last_update = self.get_last_update_date(soup)
         self.title = soup.title.string
         self.is_active = self.get_is_active(soup)
         self.pdf_url = self.get_pdf_url(soup)
@@ -83,25 +58,58 @@ class SrRecord(object):
             with open(file_name, "wb") as file:
                 file.write(pdf_file.content)
 
+    @staticmethod
+    def get_csv_header():
+        return (
+            "ID; "
+            "Title (German); "
+            "Last update; "
+            "Active status; "
+            "URL"
+            )
+
+    def to_csv(self):
+        return (
+            f"{self.sr_id}; "
+            f"{self.title}; "
+            f"{self.last_update}; "
+            f"{self.is_active}; "
+            f"{self.url}"
+            )
+
     def __str__(self):
         return (
             f"SR record with ID: {self.sr_id}\n"
-            f"Last updated on: {self.last_updated}\n"
+            f"Last updated on: {self.last_update}\n"
             f"Title: {self.title}\n"
             f"Active Status: {self.is_active}\n"
         )
 
 
 def main(args):
+
     if not args.print_only:
         if not os.path.exists(args.output_dir):
             os.makedirs(args.output_dir)
+
+    records = []
     for sr_id in args.sr_ids:
         record = SrRecord(sr_id)
         record.populate()
-        print(record)
-        if not args.print_only:
+        records.append(record)
+        logger.info(record)
+
+    logger.info(SrRecord.get_csv_header())
+    for record in records:
+        logger.info(record.to_csv())
+
+    if args.print_only:
+        logger.info("Skipping downloads.")
+    else:
+        logger.info(f"Downloading PDFs into {args.output_dir} ...")
+        for record in records:
             record.download_pdf(args.output_dir)
+        logger.info("Done.")
 
 
 if __name__ == "__main__":
@@ -110,7 +118,7 @@ if __name__ == "__main__":
                         help='IDs of documents to fetch')
     parser.add_argument('-o', '--output_dir', default=download_dir,
             help='where to place the PDF files')
-    parser.add_argument('-p', '--print_only',
+    parser.add_argument('-p', '--print_only', action='store_true',
             help='do not download anything, just print some info')
     args = parser.parse_args()
     main(args)
